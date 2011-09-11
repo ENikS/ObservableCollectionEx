6e447855b349385a5eea8a963b3bf48abcaa6c5d
ENikS_cp <SND\ENikS_cp>
2011-09-11 19:05:36 +0000

    - Resolved issue with CollectionView not being updated correctly - Added WPF test project

--- ObservableCollectionEx/Source/ObservableCollectionEx/Properties/AssemblyInfo.cs
+++ ObservableCollectionEx/Source/ObservableCollectionEx/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("ObservableCollectionEx")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("ENikS")]
 [assembly: AssemblyProduct("ObservableCollectionEx")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Eugene Sadovoi 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
